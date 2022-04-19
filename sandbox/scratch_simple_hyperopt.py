3e8900609f663c5b07cef16d9efd43bf1076c008
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-04-19 18:18:23 -0400

    Add simple example of optuna on cpcn

---
+++ sandbox/scratch_simple_hyperopt.py
@@ -0,0 +1,112 @@
+# %% [markdown]
+# # A simple test of using `optuna` for hyperparameter optimization
+
+# %%
+import time
+from types import SimpleNamespace
+
+import pydove as dv
+from tqdm.notebook import tqdm
+
+import torch
+from cpcn import LinearCPCNetwork, load_mnist, train
+
+import optuna
+from optuna.trial import TrialState
+
+# %% [markdown]
+# ## Defining the optimization
+
+# %%
+def optuna_reporter(trial: optuna.trial.Trial, ns: SimpleNamespace):
+    trial.report(ns.epoch_val_loss, ns.epoch)
+
+    # early pruning
+    if trial.should_prune():
+        raise optuna.exceptions.TrialPruned()
+
+
+def create_cpcn(trial):
+    n_hidden = trial.suggest_int("n_hidden", 1, 3)
+    dims = [28 * 28]
+    for i in range(n_hidden):
+        n_units = trial.suggest_int(f"n_units_l{i}", 4, 128)
+        dims.append(n_units)
+    dims.append(10)
+
+    z_lr = trial.suggest_float("z_lr", 1e-5, 1e-1, log=True)
+
+    # set parameters to match a simple PCN network
+    g_a = 0.5 * torch.ones(len(dims) - 2)
+    g_a[-1] *= 2
+
+    g_b = 0.5 * torch.ones(len(dims) - 2)
+    g_b[0] *= 2
+
+    net = LinearCPCNetwork(dims, z_lr=z_lr, z_it=50, g_a=g_a, g_b=g_b, c_m=0, l_s=g_b)
+
+    return net
+
+
+def objective(
+    trial: optuna.trial.Trial, n_epochs: int, dataset: dict, device: torch.device
+) -> float:
+    net = create_cpcn(trial).to(device)
+
+    optimizer_type = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+
+    optimizer_class = getattr(torch.optim, optimizer_type)
+    optimizer_kws = {"lr": lr}
+
+    results = train(
+        net,
+        n_epochs,
+        dataset["train"],
+        dataset["validation"],
+        optimizer=optimizer_class,
+        optimizer_kwargs=optimizer_kws,
+        reporter=lambda ns: optuna_reporter(trial, ns),
+    )
+
+    return results.validation.pc_loss[-1]
+
+
+# %%
+# minimizing PC loss
+t0 = time.time()
+
+device = torch.device("cpu")
+
+n_epochs = 15
+dataset = load_mnist(n_train=2000, n_validation=1000, batch_size=128)
+
+study = optuna.create_study(direction="minimize")
+study.optimize(
+    lambda trial: objective(trial, n_epochs, dataset, device),
+    n_trials=100,
+    timeout=600,
+    show_progress_bar=True,
+)
+
+pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+
+t1 = time.time()
+
+# %%
+print(
+    f"{len(study.trials)} trials in {t1 - t0:.1f} seconds: "
+    f"{len(complete_trials)} complete, {len(pruned_trials)} pruned."
+)
+
+trial = study.best_trial
+print(f"best pc_loss: {trial.value}, for params:")
+for key, value in trial.params.items():
+    print("    {}: {}".format(key, value))
+
+# %%
+
+optuna.visualization.matplotlib.plot_param_importances(study)
+
+# %%
