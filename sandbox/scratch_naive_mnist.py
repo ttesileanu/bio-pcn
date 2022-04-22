bca18aac8638d9a6e1a12a18bcfe372473ed9ba3
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-04-21 22:39:28 -0400

    Remove bias + plot error rate in naive test

--- sandbox/scratch_naive_mnist.py
+++ sandbox/scratch_naive_mnist.py
@@ -43,6 +43,7 @@ net = PCNetwork(
     lr_inference=lr_inference,
     it_inference=it_inference,
     variances=1.0,
+    bias=False,
 )
 net = net.to(device)
 
@@ -67,14 +68,14 @@ with dv.FigureManager(1, 2) as (_, (ax1, ax2)):
         f"{last_loss:.3f}", (len(results.validation.pc_loss), last_loss), c="C1"
     )
 
-    ax2.plot(100 * results.train.accuracy, label="train")
-    ax2.plot(100 * results.validation.accuracy, label="val")
+    ax2.plot(100 * (1 - results.train.accuracy), label="train")
+    ax2.plot(100 * (1 - results.validation.accuracy), label="val")
     ax2.set_xlabel("epoch")
-    ax2.set_ylabel("accuracy on validation set (%)")
+    ax2.set_ylabel("error rate (%)")
     ax2.legend(frameon=False)
-    last_acc = 100 * results.train.accuracy[-1]
+    last_acc = 100 * (1 - results.train.accuracy[-1])
     ax2.annotate(f"{last_acc:.1f}%", (len(results.train.accuracy), last_acc), c="C0")
-    last_acc = 100 * results.validation.accuracy[-1]
+    last_acc = 100 * (1 - results.validation.accuracy[-1])
     ax2.annotate(
         f"{last_acc:.1f}%", (len(results.validation.accuracy), last_acc), c="C1"
     )
@@ -97,7 +98,15 @@ g_b = 0.5 * np.ones(len(dims) - 2)
 g_b[0] *= 2
 
 cpcn_net = LinearCPCNetwork(
-    dims, z_lr=z_lr, z_it=z_it, g_a=g_a, g_b=g_b, c_m=0, l_s=g_b
+    dims,
+    z_lr=z_lr,
+    z_it=z_it,
+    g_a=g_a,
+    g_b=g_b,
+    c_m=0,
+    l_s=g_b,
+    bias_a=False,
+    bias_b=False,
 )
 cpcn_net = cpcn_net.to(device)
 
@@ -132,23 +141,23 @@ with dv.FigureManager(1, 2) as (_, (ax1, ax2)):
     ax1.legend(frameon=False)
 
     ax2.plot(
-        100 * results.train.accuracy,
+        100 * (1 - results.train.accuracy),
         c="C0",
         ls="--",
         alpha=0.7,
         label="Whittington&Bogacz",
     )
-    ax2.plot(100 * results.validation.accuracy, c="C1", ls="--", alpha=0.7)
-    ax2.plot(100 * cpcn_results.train.accuracy, c="C0", label="train")
-    ax2.plot(100 * cpcn_results.validation.accuracy, c="C1", label="val")
+    ax2.plot(100 * (1 - results.validation.accuracy), c="C1", ls="--", alpha=0.7)
+    ax2.plot(100 * (1 - cpcn_results.train.accuracy), c="C0", label="train")
+    ax2.plot(100 * (1 - cpcn_results.validation.accuracy), c="C1", label="val")
     ax2.set_xlabel("epoch")
-    ax2.set_ylabel("accuracy (%)")
+    ax2.set_ylabel("error rate (%)")
 
-    last_acc = 100 * results.validation.accuracy[-1]
+    last_acc = 100 * (1 - results.validation.accuracy[-1])
     ax2.annotate(
         f"{last_acc:.1f}%", (len(results.validation.accuracy), last_acc), c="C1"
     )
-    last_acc = 100 * cpcn_results.validation.accuracy[-1]
+    last_acc = 100 * (1 - cpcn_results.validation.accuracy[-1])
     ax2.annotate(
         f"{last_acc:.1f}%", (len(cpcn_results.validation.accuracy), last_acc), c="C1"
     )
