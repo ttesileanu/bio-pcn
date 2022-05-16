92630bd6aa0e65a5e9e18447ae2f77745e5e3b51
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-05-16 10:53:15 -0400

    Add layer option to show_constraint_diag

--- cpcn/graph.py
+++ cpcn/graph.py
@@ -13,17 +13,16 @@ from typing import Optional, Tuple, Sequence, Callable
 
 
 def show_constraint_diagnostics(
-    diagnostics: dict, n_mat: int = 4, rho: float = 1.0
+    diagnostics: dict, n_mat: int = 4, rho: float = 1.0, layer: int = 1
 ) -> plt.Figure:
     """Make a figure diagnosing the whitening constraint.
 
     Uses the diagnostics dictionary returned from `util.get_constraint_diagnostics`.
 
-    NB: This currently only works for a single hidden layer.
-
     :param diagnostics: dictionary of diagnostic measures
     :param n_mat: number of covariance matrices to display
     :param rho: limit on eigenvalues of covariance matrices
+    :param layer: which layer to focus on
     :return: the figure that was created
     """
     fig = plt.figure(figsize=(7, 7), constrained_layout=True)
@@ -41,17 +40,17 @@ def show_constraint_diagnostics(
     sel_cov = torch.linspace(0, n_cov - 1, 5).int()
 
     for ax, i in zip(mat_axs, sel_cov):
-        crt_cov = diagnostics["cov:1"][i]
+        crt_cov = diagnostics[f"cov:{layer}"][i]
         crt_scale = torch.max(torch.abs(crt_cov))
         ax.imshow(crt_cov, vmin=-crt_scale, vmax=crt_scale, cmap="RdBu_r")
         ax.set_title(f"batch {diagnostics['batch'][i]}")
 
     # draw the evolution of the trace of the constraint
     trace_ax.axhline(0, c="k", ls="--")
-    trace_ax.plot(diagnostics["batch"], diagnostics["trace:1"])
+    trace_ax.plot(diagnostics["batch"], diagnostics[f"trace:{layer}"])
     trace_ax.annotate(
-        f"{diagnostics['trace:1'][-1]:.2g}",
-        (diagnostics["batch"][-1], diagnostics["trace:1"][-1]),
+        f"{diagnostics[f'trace:{layer}'][-1]:.2g}",
+        (diagnostics["batch"][-1], diagnostics[f"trace:{layer}"][-1]),
         xytext=(3, 0),
         textcoords="offset points",
         va="center",
@@ -63,11 +62,11 @@ def show_constraint_diagnostics(
 
     # draw the evolution of the eigenvalues
     eval_ax.axhline(rho, c="k", ls="--")
-    eval_ax.plot(diagnostics["batch"], diagnostics["evals:1"], alpha=0.5, lw=1)
-    eval_ax.plot(diagnostics["batch"], diagnostics["max_eval:1"], c="k", lw=2)
+    eval_ax.plot(diagnostics["batch"], diagnostics[f"evals:{layer}"], alpha=0.5, lw=1)
+    eval_ax.plot(diagnostics["batch"], diagnostics[f"max_eval:{layer}"], c="k", lw=2)
     eval_ax.annotate(
-        f"{diagnostics['max_eval:1'][-1]:.2g}",
-        (diagnostics["batch"][-1], diagnostics["max_eval:1"][-1]),
+        f"{diagnostics[f'max_eval:{layer}'][-1]:.2g}",
+        (diagnostics["batch"][-1], diagnostics[f"max_eval:{layer}"][-1]),
         xytext=(3, 0),
         textcoords="offset points",
         va="center",
@@ -75,7 +74,7 @@ def show_constraint_diagnostics(
         fontweight="bold",
     )
     eval_ax.set_xlabel("batch")
-    eval_ax.set_ylabel("maximum $z$-cov eval")
+    eval_ax.set_ylabel("eigenvalues of $z$-cov")
     eval_ax.set_ylim(0, 5 * rho)
 
     for ax in [trace_ax, eval_ax]:
@@ -209,7 +208,7 @@ def show_weight_evolution(x: torch.Tensor, weights: torch.Tensor, ax: plt.Axes):
 
 
 def show_latent_convergence(
-    fast_results: dict, ax: Optional[plt.Axes] = None, var: str = "z"
+    fast_results: dict, ax: Optional[plt.Axes] = None, var: str = "z", layer: int = 1
 ):
     """Make plots showing how the latent variables converge.
 
@@ -219,6 +218,7 @@ def show_latent_convergence(
         `"z"` (the name can be overridden using the `var` argument; see below)
     :param ax: axes in which to make the plot; default: current axes
     :param var: name of latent variable
+    :param layer: which layer to focus on
     """
     if ax is None:
         ax = plt.gca()
@@ -230,7 +230,7 @@ def show_latent_convergence(
     crt_sel = fast_results["sample"] == max_sample
 
     batch = fast_results["batch"][crt_sel]
-    z = fast_results["z:1"][crt_sel]
+    z = fast_results[f"z:{layer}"][crt_sel]
 
     n = len(batch)
     for i in range(n):
