f7331ea207484b0e1f03cbc3ed2e8c29713446a0
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-05-19 00:58:35 -0400

    Add script to make misc figures

---
+++ draft/make_misc_figures.py
@@ -0,0 +1,39 @@
+# %% [markdown]
+# # Make some miscellaneous figures
+
+import os
+import os.path as osp
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pydove as dv
+
+import numpy as np
+import torch
+
+from cpcn import load_mnist
+
+fig_path = "figs"
+
+# %%
+
+mpl.rcParams["axes.linewidth"] = 0.5
+
+# %% [markdown]
+# ## A digit
+
+# %%
+
+mnist = load_mnist(return_loaders=False)
+
+# %%
+
+fig, ax = plt.subplots(figsize=(2, 2))
+ax.imshow(mnist["train"][0][0].reshape(28, 28), cmap="gray_r")
+ax.set_xticks([])
+ax.set_yticks([])
+
+fig.savefig(osp.join(fig_path, "mnist_digit.pdf"))
+
+# %%
