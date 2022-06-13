9c11cfe8a0723e29a9f9d637c6788dddc7716bd2
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-06-13 16:41:49 -0400

    Import PCWrapper in main namespace

--- cpcn/__init__.py
+++ cpcn/__init__.py
@@ -5,3 +5,4 @@ from .track import Tracker
 from .util import make_onehot, one_hot_accuracy, load_mnist, load_csv
 from .util import get_constraint_diagnostics, dot_accuracy
 from .train import Trainer, DivergenceError, DivergenceWarning
+from .wrapper import PCWrapper
