510e59fe47b9a93ad86a5cc2f60ee7341c9e3528
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-06-20 14:07:57 -0400

    Update profile_pcn to new framework

--- sandbox/profile_pcn.py
+++ sandbox/profile_pcn.py
@@ -4,10 +4,11 @@ from types import SimpleNamespace
 from functools import partial
 
 import torch
+import time
 
 from tqdm.notebook import tqdm
 
-from cpcn import PCNetwork, load_mnist, Trainer
+from cpcn import *
 
 # setup
 device = torch.device("cpu")
@@ -17,15 +18,16 @@ batch_size = 100
 dataset = load_mnist(n_validation=500, batch_size=batch_size, device=device)
 
 # create net, run training
-n_batches = 100
+n_batches = 2000
 dims = [784, 5, 10]
 
 z_it = 50
 z_lr = 0.1
 
+t0 = time.time()
 torch.manual_seed(123)
 
-net = PCNetwork(
+net0 = PCNetwork(
     dims,
     activation=lambda _: _,
     z_lr=z_lr,
@@ -35,12 +37,30 @@ net = PCNetwork(
     fast_optimizer=torch.optim.Adam,
     bias=False,
 )
-net = net.to(device)
 
-trainer = Trainer(net, dataset["train"], dataset["validation"])
-# trainer.set_classifier("linear")
+net = PCWrapper(net0, "linear").to(device)
+optimizer = torch.optim.Adam(net.pc_net.parameters(), lr=0.001)
+predictor_optimizer = torch.optim.Adam(net.predictor.parameters())
+lr_power = 1.0
+lr_rate = 4e-4
+scheduler = torch.optim.lr_scheduler.LambdaLR(
+    optimizer, lr_lambda=lambda batch: 1 / (1 + lr_rate * batch ** lr_power)
+)
+trainer = Trainer(dataset["train"], invalid_action="warn+stop")
+trainer.metrics["accuracy"] = dot_accuracy
+for batch in tqdmw(trainer(n_batches)):
+    if batch.every(10):
+        batch.evaluate(dataset["validation"]).run(net)
+        batch.weight.report("W", net.pc_net.W)
 
-trainer.set_optimizer(torch.optim.Adam, lr=0.001)
-# trainer.add_scheduler(partial(torch.optim.lr_scheduler.ExponentialLR, gamma=0.9))
+    ns = batch.feed(net, latent_profile=True)
+    batch.latent.report_batch("z", ns.z)
+    if batch.count(4):
+        batch.fast.report_batch("z", [_.transpose(0, 1) for _ in ns.profile.z])
 
-results = trainer.run(n_batches=n_batches)
+    optimizer.step()
+    predictor_optimizer.step()
+    scheduler.step()
+
+results = trainer.history
+print(f"Training PCN took {time.time() - t0:.1f} seconds.")
