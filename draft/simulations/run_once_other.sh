928635ac0790ad37489261b8d0bc084744f92cc8
Tiberiu Tesileanu <ttesileanu@gmail.com>
2022-07-26 16:38:58 -0400

    Add simulations for weight symm runs

---
+++ draft/simulations/run_once_other.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+source ~/.bashrc
+
+# run_once_other <seed> <dataset> <algo> <hidden1> <hidden2> <rho1> <rho2>
+
+seed=$1
+dataset=$2
+algo=$3
+hidden1=$4
+hidden2=$5
+rho1=$6
+rho2=$7
+
+arch="many_${hidden1}_${hidden2}"
+
+if [ "${algo}" = "wb" ]; then
+    folder="${dataset}_${algo}_${arch}"
+    rho_args=""
+else
+    all_rho="${rho1}_${rho2}"
+    folder="${dataset}_${algo}_${arch}_rho${all_rho}"
+    rho_args="--rho ${rho1} ${rho2}"
+fi
+
+mkdir -p ${folder}/log
+outfile="${folder}/history_${seed}.pkl"
+
+if [ -f "${outfile}" ]; then
+    echo "output file ${outfile} exists! quitting!"
+    exit 1
+fi
+
+conda activate cpcn
+
+./run_simulation.py \
+    ${rho_args} -- \
+    . \
+    ../hyper/ \
+    $dataset \
+    $algo \
+    $arch \
+    $seed \
+    > "${folder}/log/out_${seed}.txt" \
+    2> "${folder}/log/err_${seed}.txt"
